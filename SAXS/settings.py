4a949776f728518e038b61d9ec036d0760178a87
lixx11 <lxx2011011580@gmail.com>
2019-04-21 14:13:53 +0800

    add STATIC_ROOT

--- SAXS/settings.py
+++ SAXS/settings.py
@@ -119,4 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 FILE_UPLOAD_MAX_MEMORY_SIZE = 209715200
