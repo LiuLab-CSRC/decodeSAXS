8b0c470cfa42c3b71667f0d5bc28601bf6251846
DreAymi <810975814@qq.com>
2019-07-09 17:33:17 +0800

    debug 500 error v final1

--- reconstruction_web/run_zalign.py
+++ reconstruction_web/run_zalign.py
@@ -1,11 +1,11 @@
 import sys
 import zalign
-from sastbx.zernike_model import model_interface
+#from sastbx.zernike_model import model_interface
 
 pdbpath=sys.argv[1]
 
 pdbfile = '/root/sites/hhe-site/decodeSAXS/reconstruction_web/media/result/%s/out.ccp4'%pdbpath
-cavitymodel = model_interface.build_model(pdbfile, 'pdb', 20, None)
+cavitymodel = model_interface.build_model(pdbfile, 'ccp4', 20, None)
 shiftrmax=cavitymodel.rmax*0.9
 args = ['fix=/root/sites/hhe-site/decodeSAXS/reconstruction_web/media/result/%s/out.ccp4'%pdbpath, 'typef=ccp4', 'mov=/root/sites/hhe-site/decodeSAXS/reconstruction_web/media/result/%s/upload_pdb.pdb'%pdbpath, 'rmax=%f'%shiftrmax]
 zalign.run(args, '/root/sites/hhe-site/decodeSAXS/reconstruction_web/media/result/%s'%pdbpath)
