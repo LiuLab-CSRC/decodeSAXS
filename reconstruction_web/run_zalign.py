28926749decf1572b777cf5f7eef9d49da28e52c
DreAymi <810975814@qq.com>
2019-07-09 15:20:35 +0800

    debug 500 error v3

--- reconstruction_web/run_zalign.py
+++ reconstruction_web/run_zalign.py
@@ -9,8 +9,8 @@ args=parser.parse_args()
 pdbpath=args.job_id
 
 
-pdbfile = './reconstruction_web/media/result/%s/out.ccp4'%pdbpath
+pdbfile = '/root/sites/hhe-site/decodeSAXS/reconstruction_web/media/result/%s/out.ccp4'%pdbpath
 cavitymodel = model_interface.build_model(pdbfile, 'pdb', 20, None)
 shiftrmax=cavitymodel.rmax*0.9
-args = ['fix=./reconstruction_web/media/result/%s/out.ccp4'%pdbpath, 'typef=ccp4', 'mov=./reconstruction_web/media/result/%s/upload_pdb.pdb'%pdbpath, 'rmax=%f'%shiftrmax]
-zalign.run(args, './reconstruction_web/media/result/'+pdbpath)
+args = ['fix=/root/sites/hhe-site/decodeSAXS/reconstruction_web/media/result/%s/out.ccp4'%pdbpath, 'typef=ccp4', 'mov=/root/sites/hhe-site/decodeSAXS/reconstruction_web/media/result/%s/upload_pdb.pdb'%pdbpath, 'rmax=%f'%shiftrmax]
+zalign.run(args, '/root/sites/hhe-site/decodeSAXS/reconstruction_web/media/result/'+pdbpath)
