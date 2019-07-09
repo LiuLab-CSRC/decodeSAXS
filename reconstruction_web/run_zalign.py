9040fcd42d761e367847ab1196e962a01c07200e
DreAymi <810975814@qq.com>
2019-07-09 16:44:37 +0800

    debug 500 error v6

--- reconstruction_web/run_zalign.py
+++ reconstruction_web/run_zalign.py
@@ -2,10 +2,10 @@ import sys
 import zalign
 from sastbx.zernike_model import model_interface
 
-pdbpath=sys.argv[1]
+#pdbpath=sys.argv[1]
 
-pdbfile = '/root/sites/hhe-site/decodeSAXS/reconstruction_web/media/result/%s/out.ccp4'%pdbpath
+pdbfile = '/root/sites/hhe-site/decodeSAXS/reconstruction_web/media/result/1561688269/out.ccp4'
 cavitymodel = model_interface.build_model(pdbfile, 'pdb', 20, None)
 shiftrmax=cavitymodel.rmax*0.9
-args = ['fix=/root/sites/hhe-site/decodeSAXS/reconstruction_web/media/result/%s/out.ccp4'%pdbpath, 'typef=ccp4', 'mov=/root/sites/hhe-site/decodeSAXS/reconstruction_web/media/result/%s/upload_pdb.pdb'%pdbpath, 'rmax=%f'%shiftrmax]
-zalign.run(args, '/root/sites/hhe-site/decodeSAXS/reconstruction_web/media/result/'+pdbpath)
+args = ['fix=/root/sites/hhe-site/decodeSAXS/reconstruction_web/media/result/1561688269/out.ccp4', 'typef=ccp4', 'mov=/root/sites/hhe-site/decodeSAXS/reconstruction_web/media/result/1561688269/upload_pdb.pdb', 'rmax=%f'%shiftrmax]
+zalign.run(args, '/root/sites/hhe-site/decodeSAXS/reconstruction_web/media/result/1561688269')
