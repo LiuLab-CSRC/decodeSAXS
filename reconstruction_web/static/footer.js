db25067978ac92ebbc4176687b88d11002ac097f
DreAymi <810975814@qq.com>
2019-07-11 21:42:01 +0800

    add header and footer

---
+++ reconstruction_web/static/footer.js
@@ -0,0 +1,6 @@
+document.writeln("<div class=\'text\' style=\' text-align:center;\'>Building 9, East Zone, ZPark II, No.10 East Xibeiwang Road, Haidian");
+document.writeln("    District, Beijing 100193, China");
+document.writeln("</div>");
+document.writeln("<div class=\'text\' style=\' text-align:center;\'>Department of Complex Systems Division</div>");
+document.writeln("<div class=\'text\' style=\' text-align:center;\'>Home Page: <a href=\'http://liulab.csrc.ac.cn\'>http://liulab.csrc.ac.cn</a></div>");
+document.writeln("<div class=\'text\' style=\' text-align:center;\'>Other web services: <a href=\'http://liulab.csrc.ac.cn/dokuwiki/doku.php?id=web_service\'>http://liulab.csrc.ac.cn/dokuwiki/doku.php?id=web_service</a></div>");
