58ac0d0c3c72d0290ecff9d734718910bf0286bc
haohe <810975814@qq.com>
2019-11-15 10:19:22 +0800

    run on cpu

--- reconstruction_web/views.py
+++ reconstruction_web/views.py
@@ -232,9 +232,7 @@ def generatedata(request,cur_time):
         saxs_data = process_result[0]
         print saxs_data
         print tag
-        if q.empty():
-            print "queue empty"
-
+        
         iq_path="./reconstruction_web/media/result/" + cur_time + '/' + 'processed.iq'
         with open(iq_path,"w") as f2:
             for ii in range(saxs_data.shape[0]):
@@ -257,6 +255,13 @@ def generatedata(request,cur_time):
             runproj()
 
 
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr(( \
+        Header(name, 'utf-8').encode(), \
+        addr.encode('utf-8') if isinstance(addr, unicode) else addr))
+
+
 def runproj():
     global tag
     tag=False
@@ -266,6 +271,7 @@ def runproj():
     email_addr = job_info[1]
     job_nameinfo = job_info[2]
     job_fileinfo = job_info[3]
+    print job_nameinfo,job_fileinfo,email_addr
     try:
         run_stat = os.system(job_cmd)
     except:
@@ -276,70 +282,70 @@ def runproj():
             os.system("cd %s/%s && rm log.txt && rm logprint.txt && rm out.pdb && mv final_saxs.txt finalfit.txt" % (
                 resultpath, job_fileinfo))
             os.system("cd %s && tar -cvf %s.tar.gz %s && mv %s.tar.gz %s" % (resultpath, job_nameinfo, job_fileinfo,job_nameinfo,job_fileinfo))
-
-            from_addr = 'hehao1777@163.com'
-            password = 'hehao1777'
-            to_addr = email_addr
-            smtp_server = 'smtp.163.com'
-            msg = MIMEMultipart()
-            msg['From'] = _format_addr(u'decodeSAXS <%s>' % from_addr)
-            msg['To'] = _format_addr(u'Users <%s>' % to_addr)
-            msg['Subject'] = Header(u'the results of decodeSAXS', 'utf-8').encode()
-
-            # add MIMEText:
-            contenthtml="""<html><head><body><p>Thanks for using decodeSAXS, hope it helpful for you, any suggestions you can contact with us.</p><p>Your job ID is :""" + job_fileinfo + """</p><p>you can check your result here: </p><p><a href="http://liulab.csrc.ac.cn:10005/check/" mce_href="http://liulab.csrc.ac.cn:10005/check/">liulab.csrc.ac.cn:10005/check/</a></p></body></head></html>"""
-            #msg.attach(MIMEText("Thanks for using decodeSAXS, hope it helpful for you, any suggestions you can contact with us.\nYour job ID is : %s\nyou can check your result here: liulab.csrc.ac.cn:10005/check/"%job_file, 'plain'))
-            msg.attach(MIMEText(contenthtml, 'html'))
-            # add file:
-            filepath=resultpath+job_fileinfo
-            #files=os.listdir(filepath)
-            job_name=job_nameinfo+'.tar.gz'
-
-            '''
-            for filei in files:
-                if '.tar.gz' in filei:
-                    job_name=filei
-                    break
-            '''
-            with open('%s/%s'%(filepath,job_name), 'rb') as f:
-                att = MIMEBase('application', 'octet-stream')
-                att.set_payload(f.read())
-                att.add_header('Content-Disposition', 'attachment', filename = ('utf-8','','%s'%job_name))
-                encoders.encode_base64(att)
-                msg.attach(att)
-
-                #mime = MIMEBase('image', 'jpg', filename='0.jpg')
-                #mime.add_header('Content-Disposition', 'attachment', filename='0.jpg')
-                #mime.add_header('Content-ID', '<0>')
-                #mime.add_header('X-Attachment-Id', '0')
-                #mime.set_payload(f.read())
-                #encoders.encode_base64(attachfile)
-                #msg.attach(attachfile)
-                
-
-            server = smtplib.SMTP(smtp_server, 25)
-            server.set_debuglevel(1)
-            server.login(from_addr, password)
-            try:
-                server.sendmail(from_addr, [to_addr], msg.as_string())
-            except smtplib.SMTPConnectError as e:
-                print '邮件发送失败，连接失败:', e.smtp_code, e.smtp_error
-            except smtplib.SMTPAuthenticationError as e:
-                print '邮件发送失败，认证错误:', e.smtp_code, e.smtp_error
-            except smtplib.SMTPSenderRefused as e:
-                print '邮件发送失败，发件人被拒绝:', e.smtp_code, e.smtp_error
-            except smtplib.SMTPRecipientsRefused as e:
-                print '邮件发送失败，收件人被拒绝:', e.smtp_code, e.smtp_error
-            except smtplib.SMTPDataError as e:
-                print '邮件发送失败，数据接收拒绝:', e.smtp_code, e.smtp_error
-            except smtplib.SMTPException as e:
-                print '邮件发送失败, ', e.message
-            except Exception as e:
-                print '邮件发送异常, ', str(e)
-            finally:
-                server.quit()
         except:
             pass
+
+        from_addr = 'hehao1777@163.com'
+        password = 'hehao1777'
+        to_addr = email_addr
+        smtp_server = 'smtp.163.com'
+        msg = MIMEMultipart()
+        msg['From'] = _format_addr(u'decodeSAXS <%s>' % from_addr)
+        msg['To'] = _format_addr(u'Users <%s>' % to_addr)
+        msg['Subject'] = Header(u'the results of decodeSAXS', 'utf-8').encode()
+
+        # add MIMEText:
+        contenthtml="""<html><head><body><p>Thanks for using decodeSAXS, hope it helpful for you, any suggestions you can contact with us.</p><p>Your job ID is :""" + job_fileinfo + """</p><p>you can check your result here: </p><p><a href="http://liulab.csrc.ac.cn:10005/check/" mce_href="http://liulab.csrc.ac.cn:10005/check/">liulab.csrc.ac.cn:10005/check/</a></p></body></head></html>"""
+        #msg.attach(MIMEText("Thanks for using decodeSAXS, hope it helpful for you, any suggestions you can contact with us.\nYour job ID is : %s\nyou can check your result here: liulab.csrc.ac.cn:10005/check/"%job_file, 'plain'))
+        msg.attach(MIMEText(contenthtml, 'html'))
+        # add file:
+        filepath=resultpath+job_fileinfo
+        #files=os.listdir(filepath)
+        job_name=job_nameinfo+'.tar.gz'
+
+        '''
+        for filei in files:
+            if '.tar.gz' in filei:
+                job_name=filei
+                break
+        '''
+        with open('%s/%s'%(filepath,job_name), 'rb') as f:
+            att = MIMEBase('application', 'octet-stream')
+            att.set_payload(f.read())
+            att.add_header('Content-Disposition', 'attachment', filename = ('utf-8','','%s'%job_name))
+            encoders.encode_base64(att)
+            msg.attach(att)
+
+            #mime = MIMEBase('image', 'jpg', filename='0.jpg')
+            #mime.add_header('Content-Disposition', 'attachment', filename='0.jpg')
+            #mime.add_header('Content-ID', '<0>')
+            #mime.add_header('X-Attachment-Id', '0')
+            #mime.set_payload(f.read())
+            #encoders.encode_base64(attachfile)
+            #msg.attach(attachfile)
+            
+
+        server = smtplib.SMTP(smtp_server, 25)
+        server.set_debuglevel(1)
+        server.login(from_addr, password)
+        try:
+            server.sendmail(from_addr, [to_addr], msg.as_string())
+        except smtplib.SMTPConnectError as e:
+            print '邮件发送失败，连接失败:', e.smtp_code, e.smtp_error
+        except smtplib.SMTPAuthenticationError as e:
+            print '邮件发送失败，认证错误:', e.smtp_code, e.smtp_error
+        except smtplib.SMTPSenderRefused as e:
+            print '邮件发送失败，发件人被拒绝:', e.smtp_code, e.smtp_error
+        except smtplib.SMTPRecipientsRefused as e:
+            print '邮件发送失败，收件人被拒绝:', e.smtp_code, e.smtp_error
+        except smtplib.SMTPDataError as e:
+            print '邮件发送失败，数据接收拒绝:', e.smtp_code, e.smtp_error
+        except smtplib.SMTPException as e:
+            print '邮件发送失败, ', e.message
+        except Exception as e:
+            print '邮件发送异常, ', str(e)
+        finally:
+            server.quit()
     tag=True
     if not q.empty():
         runproj()
